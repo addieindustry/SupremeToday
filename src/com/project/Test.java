1cf5e04578a15151fb65dbacb1d2b40e5b1b96e4
Addie <ashish.dave@iconflux.com>
2021-03-05 12:23:38 +0530

    encryption decryption finalized

--- src/com/project/Test.java
+++ src/com/project/Test.java
@@ -1,5 +1,6 @@
 package com.project;
 
+import com.license4j.util.FileUtils;
 import com.project.utility.EncryptionHelper;
 import com.project.utility.EncryptionHelperLatest;
 
@@ -14,7 +15,25 @@ import java.util.List;
 
 public class Test {
 
+    public static byte[] integersToBytes(int[] values)
+    {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        DataOutputStream dos = new DataOutputStream(baos);
+        for(int i=0; i < values.length; ++i)
+        {
+            try {
+                dos.writeInt(values[i]);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return baos.toByteArray();
+    }
+
     public static void main (String[] arg) {
+//        EncryptionHelperLatest.EncryptDictionary();
+
         String KEY = "1234567890";
         String INPUT_TEXT = "HELLO addie, How are you Addie";
 
@@ -25,17 +44,47 @@ public class Test {
         }
 
         INPUT_TEXT = INPUT_TEXT.replaceAll("<", " <").replaceAll(">", "> ");
+        INPUT_TEXT = INPUT_TEXT.replaceAll("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;", "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; ").replaceAll("href=", "href= ");
 
 //        List<String> supreme_dictionary = new ArrayList<String>();
 //        supreme_dictionary.add("");
 
         String ENCRYPTED_TEXT = EncryptionHelperLatest.encrypt(new StringBuilder(INPUT_TEXT), KEY);
+
+//        ArrayList<Integer> ENCRYPTED_ARRAY = EncryptionHelperLatest.encrypt_binary(new StringBuilder(INPUT_TEXT), KEY);
 //        System.out.println("ENCRYPTED_TEXT");
 //        System.out.println(ENCRYPTED_TEXT);
-
 //        WriteObjectToFile(supreme_dictionary);
-//
 //        supreme_dictionary = ReadObjectToFile();
+
+//        /**/byte[] BYTES_ARR = integersToBytes(ENCRYPTED_ARRAY.stream().mapToInt(i->i).toArray());
+//        try (FileOutputStream fos = new FileOutputStream("D:\\Projects\\SupremeToday\\SupremeToday\\res\\temp_bite.html")) {
+//            fos.write(BYTES_ARR);
+//            //fos.close(); There is no more need for this line since you had created the instance of "fos" inside the try. And this will automatically close the OutputStream
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+//        OutputStream os = null;
+//        try {
+//            os = new FileOutputStream("D:\\Projects\\SupremeToday\\SupremeToday\\res\\temp_bite.html");
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        }
+//        // Starts writing the bytes in it
+//        try {
+//            os.write(BYTES_ARR);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        System.out.println("Successfully byte inserted");
+//        try {
+//            os.close();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
 
         try (Writer writer = new BufferedWriter(new OutputStreamWriter(
                 new FileOutputStream("D:\\Projects\\SupremeToday\\SupremeToday\\res\\temp_out.html"), "utf-8"))) {
--- src/com/project/utility/EncryptionHelperLatest.java
+++ src/com/project/utility/EncryptionHelperLatest.java
@@ -4,8 +4,14 @@
  * and open the template in the editor.
  */
 package com.project.utility;
+import com.license4j.util.FileUtils;
+import com.project.AES;
+
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -20,73 +26,197 @@ public class EncryptionHelperLatest {
 
     public static String sep_img = "--ImageSplitter--";
 
-    public static String supreme_dictionary_index_file_path  = "D:\\Projects\\SupremeToday\\SupremeToday\\res\\index.txt";
+    public static String secret_key = "ASHISHDAVE";
+
+    public static String start_tag = "#";
+
+    public static String end_tag = "|";
+
+    public static String plain_keyword  = "D:\\Projects\\SupremeToday\\plain_keyword.txt";
+
+    public static String st_keyword  = "D:\\Projects\\SupremeToday\\st_keyword.txt";
 
     public static List<String> supreme_dictionary  = ReadObjectToFile();
 
     public static List<String> ReadObjectToFile(){
-        File f = new File(supreme_dictionary_index_file_path);
-        if(!f.exists()) {
-            List<String> supreme_dictionary_init = new ArrayList<>();
-            supreme_dictionary_init.add("");
-//            supreme_dictionary_init.add("");
-            WriteObjectToFile(supreme_dictionary_init);
-        }
+//        File f = new File(supreme_dictionary_index_file_path);
+        List<String> supreme_dictionary_init = new ArrayList<>();
+//        supreme_dictionary_init.add("");
 
-        FileInputStream fis = null;
         try {
-            fis = new FileInputStream(supreme_dictionary_index_file_path);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        ObjectInputStream ois = null;
-        try {
-            ois = new ObjectInputStream(fis);
+            String inputFile = new String(Files.readAllBytes(Paths.get(st_keyword)));
+            String lines[] = inputFile.split("\\n");
+            for(int i=0; i<lines.length; i++){
+                supreme_dictionary_init.add(AES.decrypt(lines[i].trim().toString(), secret_key));
+//                supreme_dictionary_init.add(lines[i].trim().toString());
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        List<String> supreme_dictionary = null;
-        try {
-            supreme_dictionary = (List<String>) ois.readObject();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        try {
-            ois.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return supreme_dictionary;
+        return supreme_dictionary_init;
     }
 
-    public static void WriteObjectToFile(List<String> supreme_dictionary){
-        FileOutputStream fos = null;
+    public static void EncryptDictionary(){
+        List<String> supreme_dictionary_init = new ArrayList<>();
+        supreme_dictionary_init.add("");
+
         try {
-            fos = new FileOutputStream(supreme_dictionary_index_file_path);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        ObjectOutputStream oos = null;
-        try {
-            oos = new ObjectOutputStream(fos);
+            String inputFile = new String(Files.readAllBytes(Paths.get(plain_keyword)));
+            String lines[] = inputFile.split("\\n");
+            for(int i=0; i<lines.length; i++){
+                supreme_dictionary_init.add(lines[i].trim().toString());
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        FileWriter fw = null;
         try {
-            oos.writeObject(supreme_dictionary);
+            fw = new FileWriter(st_keyword);
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        for (int i = 0; i < supreme_dictionary_init.size(); i++) {
+            try {
+                if (i == (int) (start_tag.charAt(0))){
+                    fw.write(AES.encrypt(start_tag, secret_key) + "\n");
+
+//                    String originalString = "howtodoinjava.com";
+//                    String decryptedString = AES.decrypt(encryptedString, secretKey) ;
+                }else if (i == (int) (end_tag.charAt(0))){
+                    fw.write(AES.encrypt(end_tag, secret_key) + "\n");
+//                    fw.write(end_tag);
+                }
+                fw.write(AES.encrypt(supreme_dictionary_init.get(i).toString(), secret_key) + "\n");
+//                fw.write();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
         try {
-            oos.close();
+            fw.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+
+//        return supreme_dictionary_init;
     }
 
     public static String encrypt(StringBuilder stemp, String key) {
+        if (stemp.toString().trim().isEmpty()) {
+            return "";
+        }
+
+        Pattern p = Pattern.compile("<img.*>");
+        Matcher m = p.matcher(stemp.toString());
+        StringBuilder sbImg = new StringBuilder("");
+        int _counter = 1;
+        while (m.find()) {
+            String s = m.group(0);
+            stemp.replace(stemp.indexOf(s), stemp.indexOf(s) + s.length(), "<IMAGE" + _counter + ">");
+            _counter+=1;
+            sbImg.append(s + "|");
+        }
+
+        try {
+            int iii = 0;
+            int i = 0;
+            for (i = 0; i <= key.length() - 1; i++) {
+                iii += Integer.parseInt(key.substring(i, i + 1).toString());
+            }
+
+            ArrayList arr = new ArrayList();
+            StringBuilder cs = new StringBuilder();
+            StringBuilder acs = new StringBuilder();
+            String[] temp = stemp.toString().split("[ ]", -1);
+
+            for (i = 0; i <= temp.length - 1; i++) {
+                if (supreme_dictionary.indexOf(temp[i]) >= 0){
+                    int dictionary_index = supreme_dictionary.indexOf(temp[i]);
+//                    System.out.println("ENC | " + dictionary_index + " | " + temp[i]);
+                    acs.append((char) (dictionary_index));
+                }else{
+//                    acs.append(temp[i]);
+                    acs.append(start_tag + temp[i] + end_tag);
+                }
+            }
+//            WriteObjectToFile(supreme_dictionary);
+            if (sbImg.toString().length() > 0){
+                return acs.toString() + sep_img + sbImg.toString();
+            }else{
+                return acs.toString();
+            }
+        } catch (RuntimeException ex) {
+            throw ex;
+        }
+    }
+
+    public static String decrypt(StringBuilder sb, String key) {
+//            if (sb.toString().trim().isEmpty() || sb.toString().trim().equals(sep)) {
+//                return "";
+//            }
+            StringBuilder sb_html = new StringBuilder("");
+            StringBuilder sb_img = new StringBuilder("");
+            if (sb.toString().contains(sep_img)){
+                String[] strTemp = sb.toString().split(sep_img);
+                sb_html = new StringBuilder(strTemp[0].toString());
+                sb_img = new StringBuilder(strTemp[1].toString());
+            }else{
+                sb_html = sb;
+            }
+
+//            String[] str = sb_html.toString().split(sep);
+//            StringBuilder sb1 = new StringBuilder(str[0].toString());
+//            StringBuilder sb2 = new StringBuilder(str[1].toString());
+
+            StringBuilder sb1 = new StringBuilder(sb_html.toString());
+
+//            int iii = 0;
+            int i = 0;
+//            for (i = 0; i <= key.length() - 1; i++) {
+//                iii += Integer.parseInt(key.charAt(i) + "");
+//            }
+            StringBuilder cs = new StringBuilder();
+
+            Boolean skipPlainText = Boolean.FALSE;
+            for (i = 0; i <= sb1.length() - 1; i++) {
+                if (start_tag.equals(String.valueOf(sb1.charAt(i)))){
+                    skipPlainText = Boolean.TRUE;
+                    System.out.println(skipPlainText.toString() + " | " + sb1.charAt(i));
+                    cs.append(" ");
+                }else if (end_tag.equals(String.valueOf(sb1.charAt(i)))){
+                    skipPlainText = Boolean.FALSE;
+                    System.out.println(skipPlainText.toString() + " | " + sb1.charAt(i));
+                    cs.append(" ");
+                }else{
+                    if (skipPlainText == Boolean.FALSE){
+                        int int_Renamed = ((int) (sb1.charAt(i)));
+                        if (supreme_dictionary.stream().count() > int_Renamed){
+                            System.out.println("DEC | " + int_Renamed +  " | " + supreme_dictionary.get(int_Renamed));
+                            cs.append(" " + supreme_dictionary.get(int_Renamed));
+                        }
+                    }else{
+                        cs.append(String.valueOf(sb1.charAt(i)));
+//                        cs.append(String.valueOf(sb1.charAt(i)).replace(start_tag, " ").replace(end_tag, " "));
+                    }
+                }
+            }
+
+            String[] tempImg = sb_img.toString().split("[|]");
+            for (i = 0; i <= tempImg.length - 1; i++) {
+                if (cs.toString().contains("<IMAGE" + (i + 1) + ">")){
+                    cs.replace(cs.indexOf("<IMAGE" + (i + 1) + ">"), cs.indexOf("<IMAGE" + (i + 1) + ">") + ("<IMAGE" + (i + 1) + ">").length(), tempImg[i].toString());
+                }
+            }
+
+            return cs.toString();
+    }
+
+
+    public static String encrypt_old(StringBuilder stemp, String key) {
         if (stemp.toString().trim().isEmpty()) {
             return "";
         }
@@ -121,86 +251,118 @@ public class EncryptionHelperLatest {
                 int dictionary_index = supreme_dictionary.indexOf(temp[i]);
                 System.out.println("ENC | " + dictionary_index + " | " + temp[i]);
                 acs.append((char) (dictionary_index));
-
-                //                cs.append(Character.toChars(arr.size()));
-//                if (arr.contains(" " + temp[i]) == false) {
-//                    arr.add(" " + temp[i]);
-//                    if (supreme_dictionary.indexOf(temp[i]) < 0){
-//                        supreme_dictionary.add(temp[i]);
-//                    }
-//                    int dictionary_index = supreme_dictionary.indexOf(temp[i]);
-//                    System.out.println("ENC | " + dictionary_index + " | " + temp[i]);
-////                    acs.append((char) (dictionary_index));
-//                    cs.append(Character.toChars(arr.size()));
-//                } else {
-//                    System.out.println(temp[i]);
-//                    cs.append(Character.toChars(arr.indexOf(" " + temp[i]) + 1));
-//                }
             }
-            WriteObjectToFile(supreme_dictionary);
+//            WriteObjectToFile(supreme_dictionary);
             if (sbImg.toString().length() > 0){
                 return acs.toString() + sep_img + sbImg.toString();
             }else{
                 return acs.toString();
             }
-
-//            if (sbImg.toString().length() > 0){
-//                return cs.toString() + sep + acs.toString() + sep_img + sbImg.toString();
-//            }else{
-//                return cs.toString() + sep + acs.toString();
-//            }
         } catch (RuntimeException ex) {
             throw ex;
         }
     }
 
-    public static String decrypt(StringBuilder sb, String key) {
+    public static String decrypt_old(StringBuilder sb, String key) {
 //            if (sb.toString().trim().isEmpty() || sb.toString().trim().equals(sep)) {
 //                return "";
 //            }
-            StringBuilder sb_html = new StringBuilder("");
-            StringBuilder sb_img = new StringBuilder("");
-            if (sb.toString().contains(sep_img)){
-                String[] strTemp = sb.toString().split(sep_img);
-                sb_html = new StringBuilder(strTemp[0].toString());
-                sb_img = new StringBuilder(strTemp[1].toString());
-            }else{
-                sb_html = sb;
-            }
+        StringBuilder sb_html = new StringBuilder("");
+        StringBuilder sb_img = new StringBuilder("");
+        if (sb.toString().contains(sep_img)){
+            String[] strTemp = sb.toString().split(sep_img);
+            sb_html = new StringBuilder(strTemp[0].toString());
+            sb_img = new StringBuilder(strTemp[1].toString());
+        }else{
+            sb_html = sb;
+        }
 
 //            String[] str = sb_html.toString().split(sep);
 //            StringBuilder sb1 = new StringBuilder(str[0].toString());
 //            StringBuilder sb2 = new StringBuilder(str[1].toString());
 
-            StringBuilder sb1 = new StringBuilder(sb_html.toString());
+        StringBuilder sb1 = new StringBuilder(sb_html.toString());
 
 //            int iii = 0;
-            int i = 0;
+        int i = 0;
 //            for (i = 0; i <= key.length() - 1; i++) {
 //                iii += Integer.parseInt(key.charAt(i) + "");
 //            }
-            StringBuilder cs = new StringBuilder();
+        StringBuilder cs = new StringBuilder();
 
-            for (i = 0; i <= sb1.length() - 1; i++) {
-                int int_Renamed = ((int) (sb1.charAt(i)));
+        for (i = 0; i <= sb1.length() - 1; i++) {
+            int int_Renamed = ((int) (sb1.charAt(i)));
 
-                if (supreme_dictionary.stream().count() > int_Renamed){
-                    System.out.println("DEC | " + int_Renamed +  " | " + supreme_dictionary.get(int_Renamed));
-                    cs.append(" " + supreme_dictionary.get(int_Renamed));
-                }
+            if (supreme_dictionary.stream().count() > int_Renamed){
+                System.out.println("DEC | " + int_Renamed +  " | " + supreme_dictionary.get(int_Renamed));
+                cs.append(" " + supreme_dictionary.get(int_Renamed));
+            }
 //                cs.append(" " + supreme_dictionary.get(int_Renamed));
 //                if (sb2.toString().length() > int_Renamed){
 //                    cs.append(" " + supreme_dictionary.get(int_Renamed));
 //                }
-            }
+        }
 
-            String[] tempImg = sb_img.toString().split("[|]");
-            for (i = 0; i <= tempImg.length - 1; i++) {
-                if (cs.toString().contains("<IMAGE" + (i + 1) + ">")){
-                    cs.replace(cs.indexOf("<IMAGE" + (i + 1) + ">"), cs.indexOf("<IMAGE" + (i + 1) + ">") + ("<IMAGE" + (i + 1) + ">").length(), tempImg[i].toString());
-                }
+        String[] tempImg = sb_img.toString().split("[|]");
+        for (i = 0; i <= tempImg.length - 1; i++) {
+            if (cs.toString().contains("<IMAGE" + (i + 1) + ">")){
+                cs.replace(cs.indexOf("<IMAGE" + (i + 1) + ">"), cs.indexOf("<IMAGE" + (i + 1) + ">") + ("<IMAGE" + (i + 1) + ">").length(), tempImg[i].toString());
             }
+        }
 
-            return cs.toString();
+        return cs.toString();
     }
+
+    //    public static ArrayList<Integer> encrypt_binary(StringBuilder stemp, String key) {
+////        if (stemp.toString().trim().isEmpty()) {
+////            return "";
+////        }
+//
+//        Pattern p = Pattern.compile("<img.*>");
+//        Matcher m = p.matcher(stemp.toString());
+//        StringBuilder sbImg = new StringBuilder("");
+//        int _counter = 1;
+//        while (m.find()) {
+//            String s = m.group(0);
+//            stemp.replace(stemp.indexOf(s), stemp.indexOf(s) + s.length(), "<IMAGE" + _counter + ">");
+//            _counter+=1;
+//            sbImg.append(s + "|");
+//        }
+//
+//        try {
+//            int iii = 0;
+//            int i = 0;
+//            for (i = 0; i <= key.length() - 1; i++) {
+//                iii += Integer.parseInt(key.substring(i, i + 1).toString());
+//            }
+//
+//            ArrayList arr = new ArrayList();
+//            StringBuilder cs = new StringBuilder();
+//            StringBuilder acs = new StringBuilder();
+//            String[] temp = stemp.toString().split("[ ]", -1);
+//
+//            ArrayList<Integer> arl = new ArrayList<Integer>();
+//
+//
+//            for (i = 0; i <= temp.length - 1; i++) {
+//                if (supreme_dictionary.indexOf(temp[i]) < 0){
+//                    supreme_dictionary.add(temp[i]);
+//                }
+//                int dictionary_index = supreme_dictionary.indexOf(temp[i]);
+//                System.out.println("ENC | " + dictionary_index + " | " + temp[i]);
+//                arl.add(dictionary_index);
+////                acs.append((char) (dictionary_index));
+//            }
+//            WriteObjectToFile(supreme_dictionary);
+//            return arl;
+////            if (sbImg.toString().length() > 0){
+////                return acs.toString() + sep_img + sbImg.toString();
+////            }else{
+////                return acs.toString();
+////            }
+//        } catch (RuntimeException ex) {
+//            throw ex;
+//        }
+//    }
+
 }
